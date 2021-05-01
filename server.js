959887cfc79e2323effc8d777d8ed1db7826c8f1
Amita <panwar.amita@gmail.com>
2021-05-01 18:35:54 -0400

    update

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
 }
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
